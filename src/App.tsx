261d40d28bb28219966084c5428b13dcd9e2277f
danield770 <dddavid2@gmail.com>
2021-07-06 21:52:24 +0300

    add header to apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { HomePage } from './app/containers/HomePage';
-// import logo from './logo.svg';
-// import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
--- src/app/graphql/index.ts
+++ src/app/graphql/index.ts
@@ -1,6 +1,28 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+require('dotenv').config();
+
+console.log('node env', process.env.NODE_ENV);
+
+const TOKEN = process.env.REACT_APP_PAT;
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // return the headers to the context so httpLink can read them
+  console.log('token: ', TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${TOKEN}`,
+    },
+  };
+});
 
 export const apolloClient = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
